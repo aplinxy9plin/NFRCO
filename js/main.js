91818ee79fd0aecde7e8f9456d872aa7dacab149
AplinXy9plin <evilberry@icloud.com>
2019-06-19 18:36:04 +0700

    first half of desktop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 $(function() {
     // UIkit.offcanvas(document.getElementById("sidebar")).show();
+    // $('.carousel').owlCarousel({
+    //     items: 5,
+    //     addClassActive: true
+    // });
 });
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1.5,
+        addClassActive: true,
+        margin: 500,
+        stagePadding: 200,
+        onDragged: callbackDot
+    });
+
+    $(".owl-carousel-act").owlCarousel({
+        items: 2,
+        addClassActive: true,
+        margin: 500,
+        stagePadding: 200,
+        onDragged: callbackDot
+    });
+
+    $(".owl-pagination").css("position", "absolute");
+    $(".owl-pagination").css("marginLeft", "50px");
+    $(".owl-pagination").css("top", "210px");
+    
+    $(".owl-item").css("marginLeft", "50px")
+    // 2851c1
+});
+function callbackDot(event) {
+    console.log("hello")
+    $('.owl-page.active span').css("background", 'red')
+}
